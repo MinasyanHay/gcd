94a656ccefdb203a5444c50dcdd553c5ae5b39e5
MinasyanHay <87180187+MinasyanHay@users.noreply.github.com>
2021-07-12 09:55:05 +0400

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,15 @@
+#include "gcd.hpp"
+
+int gcd(int a,int b)
+{
+	if(a==b)
+	{
+	return a;
+	}
+	else if (a>b)
+	{
+		return gcd(a-b,b);
+	}
+	else 
+		return gcd(a,b-a);
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "gcd.hpp"
+
+int main()
+{
+	int a=0, b=0;
+	cout<<"Input numbers: a=";cin>>a;cout<<":  b=";cin>>b;
+	cout<<"GCD="<<gcd(a,b);
+	return 0;
+}
